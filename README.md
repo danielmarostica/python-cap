02f9cb09dac1fca04752fd4f009de47f21885fa0
Daniel <danielmkeras@gmail.com>
2020-08-03 17:00:35 -0300

    implemented accuracy ratio

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Cumulative Accuracy Profile (CAP)
 This python code is used to evaluate the performance of a binary classificator model.
-It takes as input three columns: usedID, groundtruth_values, predicted_values.
-Returns a visual representation of model performance.
-### Important! Sort your csv file by groundtruth_values!
+It takes as input three columns, in this order: usedID, groundtruth_values, predicted_values.
+
+Returns a visual representation of model performance with accuracy ratio (AUC).
+
+<p align="center"> 
+    <img src="cap.jpg" align="left" width="10px"></img>
+</p>
 
 Tags: Machine Learning, Python, Binary Classifier
--- cap.py
+++ cap.py
@@ -1,32 +1,65 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import *
+from sklearn.metrics import auc
+import numpy as np
 
 #https://github.com/danielmarostica/python-cap
 
 rc('text', usetex=True)
 rc('font',**{'family':'serif','serif':['Times']})
 
-#Load the data
+# load the data
 data = pd.read_csv('data.csv')
+y = data[data.columns[1]]
 
-#Do some magic
-total_1 = len(data[data['Survived'] == 1])
-total_records = len(data.index)
-percent_of_1 = total_1/total_records
-percent_of_total_records = [(i+1)/total_records for i in range(total_records)]
-random_select = [(i+1)*percent_of_1 for i in range(total_records)]
-random_select_percent_of_1 = [random_select[i]/total_1 for i in range(total_records)]
-model_select = [sum(data.iloc[0:i+1,1]) for i in range(total_records)]
-model_select_percent_of_1 = [model_select[i]/total_1 for i in range(total_records)]
+# sort
+data_sorted_ypred = data.sort_values(by=[data.columns[2]], ascending=False)
+data_sorted_y = data.sort_values(by=[data.columns[1]], ascending=False)
+
+# total records
+total_records = len(data)
+# total amount of positives
+total_positive = len(data[data[data.columns[1]] == 1])
+
+# proportion of the total records (x axis)
+x = [(i+1)/total_records for i in range(total_records)]
+
+# proportion of positives out of total
+proportion_of_positive = total_positive/total_records
+# random select
+random_select = [(i+1)*proportion_of_positive for i in range(total_records)]
+# out of the random select, proportion of positives (y axis)
+random_select_proportion_of_positive = [random_select[i]/total_positive for i in range(total_records)]
+
+# model select
+model_select = [sum(data_sorted_ypred.iloc[0:i+1,1]) for i in range(total_records)]
+# out of the model select, proportion of positives (y axis)
+model_select_proportion_of_positive = [model_select[i]/total_positive for i in range(total_records)]
+
+# perfect select
+perfect_select = [sum(data_sorted_y.iloc[0:i+1,1]) for i in range(total_records)]
+# out of the perfect select, proportion of positives (y axis)
+perfect_select_proportion_of_positive = [perfect_select[i]/total_positive for i in range(total_records)]
+
+auc_random = auc(x, random_select_proportion_of_positive)
+auc_model = auc(x, model_select_proportion_of_positive)
+auc_perfect = auc(x, perfect_select_proportion_of_positive)
+
+acc_ratio = (auc_model-auc_random)/(auc_perfect-auc_random)
+
+#print(auc_perfect)
+#x = np.arange(len(data['Survived']))/len(data['Survived'])
 
 #Voilà
-plt.plot(percent_of_total_records, random_select_percent_of_1, color='red', label='Random')
-plt.plot(percent_of_total_records, model_select_percent_of_1, color='green', label='Model')
+plt.plot(x, random_select_proportion_of_positive, color='red', label='Random')
+plt.plot(x, model_select_proportion_of_positive, color='green', label='Model')
+plt.plot(x, perfect_select_proportion_of_positive, color='blue', label='Perfect')
+plt.text(1,0,'Accuracy Ratio: {:.2f}'.format(acc_ratio), ha='right')
 plt.title('Cumulative Accuracy Profile (CAP)')
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
+plt.legend(frameon=False)
 plt.savefig('cap.jpg', dpi=150)
-plt.legend()
 #plt.show()
 plt.close()
